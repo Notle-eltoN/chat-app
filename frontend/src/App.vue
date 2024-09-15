a58338fdf38cbfb0001ec4df0d432eb03fd4e28e
Elton Chang <80650461+Notle-eltoN@users.noreply.github.com>
2024-09-14 21:59:37 -0700

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,31 @@
 <template>
-    <div id="app">
-      <router-view />
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'App',
-  };
-  </script>
-  
+  <div id="app">
+    <!-- Vue Router will inject the matched component here -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+  margin: 0;
+  padding: 0;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #f4f4f4;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
